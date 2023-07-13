71a7afb0c7936c1374af6ad2be095dd306e48800
Victor Pereira <vpereira@suse.com>
2023-07-13 15:06:38 +0200

    Check if PRs are open regardless of the working mode

--- spec/gitlab_check_merge_request_spec.rb
+++ spec/gitlab_check_merge_request_spec.rb
@@ -20,4 +20,17 @@ describe GitlabCheckMergeRequest do
 
     it { _(gcmr.pr_exist?).must_equal true }
   end
+
+  describe '#pr_to_gem_exist?' do
+    let(:gcmr) do
+      GitlabCheckMergeRequest.new(token: 'foo', gem: 'rails', working_mode: 'minor', gitlab_url: 'https://example.org')
+    end
+
+    before do
+      gcmr.stubs(:get_all_prs).returns(JsonPayload)
+    end
+
+    it { _(gcmr.pr_exist?).must_equal false }
+    it { _(gcmr.pr_to_gem_exist?).must_equal true }
+  end
 end
