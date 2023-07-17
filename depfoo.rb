ccbb9de2191a58ff71c4ff86ddf471c902bd1d82
Victor Pereira <vpereira@suse.com>
2023-07-17 14:32:41 +0200

    Fix class name

--- depfoo.rb
+++ depfoo.rb
@@ -32,8 +32,8 @@ Depfoo::OutdatedGems.new(working_mode: working_mode).call.each do |gem|
   # close related PRs
   if gpre.pr_to_gem_exist?
     gpre.related_open_prs(check_working_mode: false).each do |pr|
-      Depfoo::CloseMergeRequest.new(gitlab_url: depfoo_config.gitlab_full_url, token: depfoo_config.private_token,
-                                    merge_request_id: pr['iid']).call
+      Depfoo::GitlabCloseMergeRequest.new(gitlab_url: depfoo_config.gitlab_full_url, token: depfoo_config.private_token,
+                                          merge_request_id: pr['iid']).call
     end
   end
 
